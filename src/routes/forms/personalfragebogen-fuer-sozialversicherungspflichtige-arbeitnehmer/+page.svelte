96419f8d9681bb48cfea63819bf27d02405d093d
MoinJulian <jh@moinjulian.com>
2023-09-24 20:44:45 +0100

    Add a asteriks to all required fields

--- src/routes/forms/personalfragebogen-fuer-sozialversicherungspflichtige-arbeitnehmer/+page.svelte
+++ src/routes/forms/personalfragebogen-fuer-sozialversicherungspflichtige-arbeitnehmer/+page.svelte
@@ -4,59 +4,61 @@
 
 <h1>Personalfragebogen für sozialversicherungspflichtige Arbeitnhemer</h1>
 
+<p class="error">Felder, die mit einem * makiert wurden, sind Pflichtfelder.</p>
+
 <form method="POST" autocomplete="on" use:enhance>
 	<div class="number">
-		<label for="mandaten-nummer">Mandatennummer</label>
+		<label for="mandaten-nummer" class="required-label">Mandatennummer</label>
 		<input type="number" name="mandaten-nummer" id="mandaten-nummer" required />
 	</div>
 	<div class="number">
-		<label for="personal-number">Personalnummer</label>
+		<label for="personal-number" class="required-label">Personalnummer</label>
 		<input type="number" name="personal-number" id="personal-number" required />
 	</div>
 	<div class="text">
-		<label for="arbeitgeber">Name des Arbeitgebers</label>
+		<label for="arbeitgeber" class="required-label">Name des Arbeitgebers</label>
 		<input type="text" name="arbeitgeber" id="arbeitgeber" required />
 	</div>
 
 	<h2>Persöhnliche angaben</h2>
 
 	<div class="text">
-		<label for="familienname">Familienname ggf. Geburtsname</label>
+		<label for="familienname" class="required-label">Familienname ggf. Geburtsname</label>
 		<input type="text" name="familienname" id="familienname" required />
 	</div>
 
 	<div class="text">
-		<label for="vorname">Vorname</label>
+		<label for="vorname" class="required-label">Vorname</label>
 		<input type="text" name="vorname" id="vorname" required />
 	</div>
 
 	<div class="text">
-		<label for="anschrift">Straße, Hausnummer inkl. Anschriftenzusatz</label>
+		<label for="anschrift" class="required-label">Straße, Hausnummer inkl. Anschriftenzusatz</label>
 		<input type="text" name="anschrift" id="anschrift" required />
 	</div>
 
 	<div class="text">
-		<label for="plz">PLZ, Ort</label>
+		<label for="plz" class="required-label">PLZ, Ort</label>
 		<input type="text" name="plz" id="plz" required />
 	</div>
 
 	<div class="date">
-		<label for="geburtstag">Geburtsdatum</label>
+		<label for="geburtstag" class="required-label">Geburtsdatum</label>
 		<input type="date" name="geburtstag" id="geburtstag" required />
 	</div>
 
 	<div class="text">
-		<label for="geburtsort">Geburtsort</label>
+		<label for="geburtsort" class="required-label">Geburtsort</label>
 		<input type="text" name="geburtsort" id="geburtsort" required />
 	</div>
 
 	<div class="text">
-		<label for="geburtsland">Geburtsland</label>
+		<label for="geburtsland" class="required-label">Geburtsland</label>
 		<input type="text" name="geburtsland" id="geburtsland" required />
 	</div>
 
 	<div class="choice">
-		<label for="familienstand">Familienstand</label>
+		<label for="familienstand" class="required-label">Familienstand</label>
 		<select name="familienstand" id="familienstand" required>
 			<option value="verheiratet">Verheiratet</option>
 			<option value="nicht-verheiratet">Nicht Verheiratet</option>
@@ -64,7 +66,7 @@
 	</div>
 
 	<div class="choice">
-		<label for="geschlecht">Geschlecht</label>
+		<label for="geschlecht" class="required-label">Geschlecht</label>
 		<select name="geschlecht" id="geschlecht" required>
 			<option value="männlich">Männlich</option>
 			<option value="weiblich">Weiblich</option>
@@ -74,12 +76,12 @@
 	</div>
 
 	<div class="text">
-		<label for="staatsangehörigkeit">Staatsangehörigkeit</label>
+		<label for="staatsangehörigkeit" class="required-label">Staatsangehörigkeit</label>
 		<input type="text" name="staatsangehörigkeit" id="staatsangehörigkeit" required />
 	</div>
 
 	<div class="telefon">
-		<label for="telefonnummer">Telefonnummer</label>
+		<label for="telefonnummer" class="required-label">Telefonnummer</label>
 		<input type="tel" name="telefonnummer" id="telefonnummer" required />
 	</div>
 
@@ -89,5 +91,10 @@
 <style lang="scss">
 	label {
 		font-size: var(--medium-font);
+	}
+
+	.required-label::after {
+		content: ' * ';
+		color: red;
 	}
 </style>
