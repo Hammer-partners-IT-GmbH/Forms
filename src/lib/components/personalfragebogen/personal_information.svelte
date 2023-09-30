9a8174731554d06aee66ca1c85dd911f25f1842b
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-09-30 13:52:44 +0100

    Start personal information

---
+++ src/lib/components/personalfragebogen/personal_information.svelte
@@ -0,0 +1,77 @@
+<div class="number">
+	<label for="mandaten-nummer" class="required-label">Mandatennummer</label>
+	<input type="number" name="mandaten-nummer" id="mandaten-nummer" required />
+</div>
+<div class="number">
+	<label for="personal-number" class="required-label">Personalnummer</label>
+	<input type="number" name="personal-number" id="personal-number" required />
+</div>
+<div class="text">
+	<label for="arbeitgeber" class="required-label">Name des Arbeitgebers</label>
+	<input type="text" name="arbeitgeber" id="arbeitgeber" required />
+</div>
+
+<h2>Persöhnliche angaben</h2>
+
+<div class="text">
+	<label for="familienname" class="required-label">Familienname ggf. Geburtsname</label>
+	<input type="text" name="familienname" id="familienname" required />
+</div>
+
+<div class="text">
+	<label for="vorname" class="required-label">Vorname</label>
+	<input type="text" name="vorname" id="vorname" required />
+</div>
+
+<div class="text">
+	<label for="anschrift" class="required-label">Straße, Hausnummer inkl. Anschriftenzusatz</label>
+	<input type="text" name="anschrift" id="anschrift" required />
+</div>
+
+<div class="text">
+	<label for="plz" class="required-label">PLZ, Ort</label>
+	<input type="text" name="plz" id="plz" required />
+</div>
+
+<div class="date">
+	<label for="geburtstag" class="required-label">Geburtsdatum</label>
+	<input type="date" name="geburtstag" id="geburtstag" required />
+</div>
+
+<div class="text">
+	<label for="geburtsort" class="required-label">Geburtsort</label>
+	<input type="text" name="geburtsort" id="geburtsort" required />
+</div>
+
+<div class="text">
+	<label for="geburtsland" class="required-label">Geburtsland</label>
+	<input type="text" name="geburtsland" id="geburtsland" required />
+</div>
+
+<div class="choice">
+	<label for="familienstand" class="required-label">Familienstand</label>
+	<select name="familienstand" id="familienstand" required>
+		<option value="verheiratet">Verheiratet</option>
+		<option value="nicht-verheiratet">Nicht Verheiratet</option>
+	</select>
+</div>
+
+<div class="choice">
+	<label for="geschlecht" class="required-label">Geschlecht</label>
+	<select name="geschlecht" id="geschlecht" required>
+		<option value="männlich">Männlich</option>
+		<option value="weiblich">Weiblich</option>
+		<option value="unbestimmt">Unbestimmt</option>
+		<option value="divers">Divers</option>
+	</select>
+</div>
+
+<div class="text">
+	<label for="staatsangehörigkeit" class="required-label">Staatsangehörigkeit</label>
+	<input type="text" name="staatsangehörigkeit" id="staatsangehörigkeit" required />
+</div>
+
+<div class="telefon">
+	<label for="telefonnummer" class="required-label">Telefonnummer</label>
+	<input type="tel" name="telefonnummer" id="telefonnummer" required />
+</div>
